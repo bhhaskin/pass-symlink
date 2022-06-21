b75063429a20b54c20a7011e4c7156a16a35a382
Lukas Engelter <lukas.engelter@netgo.de>
2022-06-21 14:57:49 +0200

    refactor: move input data tests to the top
    This helps to generalize the actual work for directories and files
    in the next commit.

--- src/symlink.bash
+++ src/symlink.bash
@@ -12,21 +12,24 @@ local parent_path=$(echo "$targetfile" | sed -e "s|[^/]||g" -e "s|/|../|g")
 
 
 if [[ -f "$PREFIX/$passfile" ]]; then
-    if [[ ! -d $targetDirectory ]]; then
-        mkdir -p "$targetDirectory"
-    fi
-    cd "$targetDirectory"
-    if [[ -f "$PREFIX/$targetfile" ]]; then
-        die "Error: $targetPath already exists"
-    elif [[ -z $targetPath ]]; then
-        die "Error: Target path is missing"
-    fi
-    ln -s "$parent_path$passfile" "$symFile" || exit $?
-    if [[ -d "$PREFIX/.git" ]]; then
-        git add . && git commit -am "Created symlink of \"$sourcePath\" as \"$targetPath\"" --quiet
-    fi
+    # everything is fine
 elif [[ -z $sourcePath ]]; then
     die "Error: Source path is missing"
 else
     die "Error: $sourcePath is not in the password store."
+fi
+
+if [[ -f "$PREFIX/$targetfile" ]]; then
+    die "Error: $targetPath already exists"
+elif [[ -z $targetPath ]]; then
+    die "Error: Target path is missing"
+fi
+
+if [[ ! -d $targetDirectory ]]; then
+    mkdir -p "$targetDirectory"
+fi
+cd "$targetDirectory"
+ln -s "$parent_path$passfile" "$symFile" || exit $?
+if [[ -d "$PREFIX/.git" ]]; then
+    git add . && git commit -am "Created symlink of \"$sourcePath\" as \"$targetPath\"" --quiet
 fi
